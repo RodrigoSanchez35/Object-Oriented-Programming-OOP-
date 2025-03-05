31b2b3a634dc138d33a59fa68d60cb4b7e07cacf
RodrigoSanchez35 <165536509+RodrigoSanchez35@users.noreply.github.com>
2025-03-04 17:31:28 -0700

    Update CargoVehicle.java

--- src/models/CargoVehicle.java
+++ src/models/CargoVehicle.java
@@ -1,1 +1,31 @@
-dfbvgvb
+package models;
+
+/**
+ * Class representing a cargo vehicle.
+ */
+public class CargoVehicle extends Vehicle {
+    private double loadCapacity; // Load capacity in tons
+
+    // Constructor
+    public CargoVehicle(String licensePlate, String brand, String model, double rentalPricePerDay, double loadCapacity) {
+        super(licensePlate, brand, model, rentalPricePerDay);
+        this.loadCapacity = loadCapacity;
+    }
+
+    // Getter and Setter
+    public double getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    public void setLoadCapacity(double loadCapacity) {
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayVehicleInfo() {
+        System.out.println("Cargo Vehicle - License Plate: " + licensePlate);
+        System.out.println("Brand: " + brand + ", Model: " + model);
+        System.out.println("Rental Price per Day: $" + rentalPricePerDay);
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
