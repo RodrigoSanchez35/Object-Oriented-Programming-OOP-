1c9de01e1c021b7f1c067b9c27cf42c93e019cee
RodrigoSanchez35 <165536509+RodrigoSanchez35@users.noreply.github.com>
2025-03-04 17:33:29 -0700

    Update Rentable.java

--- src/models/Rentable.java
+++ src/models/Rentable.java
@@ -1,1 +1,16 @@
-fgbcfcg
+package models;
+
+/**
+ * Interface defining rental calculations.
+ */
+public interface Rentable {
+    double IVA = 0.19;
+    double CARGO_DISCOUNT = 0.03;
+    double PASSENGER_DISCOUNT = 0.07;
+
+    /**
+     * Method to calculate the rental invoice.
+     * @return Total cost including IVA and discounts.
+     */
+    double calculateRentalInvoice();
+}
